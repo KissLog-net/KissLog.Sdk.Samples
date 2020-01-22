d5d48b1c3beb9a9ba6e8f0cddd4cc5c145abd5e6
catalin.gavan <catalin.gavan@additiv.ch>
2020-01-22 15:49:02 +0200

    added ASP.NET MVC sample

---
+++ src/KissLog.Samples.Mvc/KissLog.Samples.Mvc/Models/IndexViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace KissLog.Samples.Mvc.Models
+{
+    public class IndexViewModel
+    {
+        public string KissLogRequestLogsUrl { get; set; }
+        public string LocalTextFilesPath { get; set; }
+    }
+}
