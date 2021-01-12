6592c972116e03973bc40297828b94713ef72a73
Catalin Gavan <catalin.gavan@ef.com>
2021-01-12 17:16:39 +0100

    implemented NLog MVC sample

---
+++ src/NLog-AspNet-MVC/NLog-AspNet-MVC/ActionFilters/TrackExecutionTimeAttribute.cs
@@ -0,0 +1,44 @@
+﻿using NLog;
+using System;
+using System.Diagnostics;
+using System.Web.Mvc;
+
+namespace NLog_AspNet_MVC.ActionFilters
+{
+    public class TrackExecutionTimeAttribute : ActionFilterAttribute
+    {
+        private readonly Stopwatch _sw;
+        private readonly Random _random;
+        public TrackExecutionTimeAttribute()
+        {
+            _sw = new Stopwatch();
+            _random = new Random();
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            ILogger logger = LogManager.GetCurrentClassLogger();
+
+            logger.Trace("TrackExecutionTimeAttribute begin");
+
+            _sw.Restart();
+
+            int sleep = _random.Next(100, 500);
+
+            System.Threading.Thread.Sleep(sleep);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            ILogger logger = LogManager.GetCurrentClassLogger();
+
+            _sw.Stop();
+
+            long elapsedMs = _sw.ElapsedMilliseconds;
+
+            filterContext.HttpContext.Response.Headers.Add("X-ElapsedMilliseconds", elapsedMs.ToString());
+
+            logger.Trace($"TrackExecutionTimeAttribute complete. Took: {elapsedMs}ms.");
+        }
+    }
+}
