53c6391d38d63b2ff51a508c625dd742750df007
Catalin Gavan <catalin.gavan@ef.com>
2021-01-13 11:20:19 +0100

    updated NLog samples

--- src/NLog-AspNet-WebApi/NLog-AspNet-WebApi/Controllers/ValuesController.cs
+++ src/NLog-AspNet-WebApi/NLog-AspNet-WebApi/Controllers/ValuesController.cs
@@ -4,14 +4,30 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using NLog;
 
 namespace NLog_AspNet_WebApi.Controllers
 {
     public class ValuesController : ApiController
     {
+        private readonly ILogger _logger;
+        public ValuesController()
+        {
+            _logger = LogManager.GetCurrentClassLogger();
+        }
+
         // GET api/values
         public IEnumerable<string> Get()
         {
+            _logger.Info("Hello world from NLog!");
+
+            _logger.Trace("Trace message");
+            _logger.Debug("Debug message");
+            _logger.Info("Info message");
+            _logger.Warn("Warning message");
+            _logger.Error("Error message");
+            _logger.Fatal("Fatal message");
+
             return new string[] { "value1", "value2" };
         }
 
