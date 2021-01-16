59cc6c8f55a316c72c9dbe9560705441f61d930c
Catalin Gavan <catalingavan@gmail.com>
2021-01-17 00:07:55 +0200

    NLog-AspNet-WebApi

--- src/NLog-AspNet-WebApi/NLog-AspNet-WebApi/Controllers/ValuesController.cs
+++ src/NLog-AspNet-WebApi/NLog-AspNet-WebApi/Controllers/ValuesController.cs
@@ -1,10 +1,6 @@
-﻿using System;
+﻿using NLog;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
-using NLog;
 
 namespace NLog_AspNet_WebApi.Controllers
 {
