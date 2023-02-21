3372b82b4c994270c64dcba0d555f5d85e014d5b
Catalin Gavan <catalingavan@gmail.com>
2023-02-21 09:25:07 +0100

    created dotnetcore 3.1 WebApp sample

---
+++ src/dotnetcore_3.1_WebApp/dotnetcore_3.1_WebApp/ActionFilters/TrackExecutionTimeAttribute.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+
+namespace dotnetcore_3._1_WebApp.ActionFilters
+{
+    public class TrackExecutionTimeAttribute : TypeFilterAttribute
+    {
+        public TrackExecutionTimeAttribute() : base(typeof(TrackExecutionTimeAttributeImpl))
+        {
+        }
+
+        private class TrackExecutionTimeAttributeImpl : IActionFilter
+        {
+            private readonly ILogger<TrackExecutionTimeAttributeImpl> _logger;
+            private readonly Stopwatch _sw;
+            private readonly Random _random;
+            public TrackExecutionTimeAttributeImpl(
+                ILogger<TrackExecutionTimeAttributeImpl> logger)
+            {
+                _logger = logger;
+                _sw = new Stopwatch();
+                _random = new Random();
+            }
+
+            public void OnActionExecuting(ActionExecutingContext context)
+            {
+                _logger.LogDebug("TrackExecutionTimeAttribute begin");
+
+                _sw.Restart();
+
+                int sleep = _random.Next(100, 500);
+
+                System.Threading.Thread.Sleep(sleep);
+            }
+
+            public void OnActionExecuted(ActionExecutedContext context)
+            {
+                _sw.Stop();
+
+                context.HttpContext.Response.Headers.Add("X-ElapsedMilliseconds", _sw.ElapsedMilliseconds.ToString());
+
+                _logger.LogDebug("TrackExecutionTimeAttribute complete. Took: {ElapsedMs}ms.", _sw.ElapsedMilliseconds);
+            }
+        }
+    }
+}
---
+++ src/dotnetcore_3.1_WebApp/dotnetcore_3.1_WebApp/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using dotnetcore_3._1_WebApp.ActionFilters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Text;
+
+namespace dotnetcore_3._1_WebApp.Controllers
+{
+    [TrackExecutionTime]
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(
+            IConfiguration configuration,
+            ILogger<HomeController> logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogTrace("Trace message");
+            _logger.LogDebug("Debug message");
+            _logger.LogInformation("Info message");
+            _logger.LogWarning("Warning message");
+            _logger.LogError("Error message");
+            _logger.LogCritical("Critical message");
+            _logger.LogError(new NullReferenceException(), "An exception");
+
+            string applicationId = _configuration["KissLog.ApplicationId"];
+            string logsUrl = $"https://kisslog.net/RequestLogs/{applicationId}/kisslogsampleapp";
+            string textLogs = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs");
+
+            var sb = new StringBuilder();
+            sb.AppendLine("KissLog.net logs:");
+            sb.AppendLine(logsUrl);
+            sb.AppendLine();
+            sb.AppendLine("File logs:");
+            sb.AppendLine(textLogs);
+
+            return Content(sb.ToString());
+        }
+    }
+}
