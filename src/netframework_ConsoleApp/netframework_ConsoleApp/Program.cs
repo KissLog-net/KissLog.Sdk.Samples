0cd4e997810b3ecf27e114465f2abd4082a87df8
Catalin Gavan <catalingavan@gmail.com>
2023-02-21 12:14:02 +0100

    created dotnetframework ConsoleApp

---
+++ src/netframework_ConsoleApp/netframework_ConsoleApp/Program.cs
@@ -0,0 +1,90 @@
+﻿using KissLog;
+using KissLog.CloudListeners.Auth;
+using KissLog.CloudListeners.RequestLogsListener;
+using KissLog.Listeners.FileListener;
+using netframework_ConsoleApp.Services;
+using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace netframework_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.SetFactory(new LoggerFactory(new Logger(url: "Program/Main")));
+
+            ConfigureKissLog();
+
+            IKLogger logger = Logger.Factory.Get();
+
+            logger.Trace("Trace log");
+            logger.Debug("Debug log");
+            logger.Info("Information log");
+            logger.Warn("Warning log");
+            logger.Error("Error log");
+            logger.Critical("Critical log");
+            logger.Error(new NullReferenceException());
+
+            IFooService fooService = new FooService(logger);
+            fooService.Foo();
+
+            Console.WriteLine(CreateMessage());
+
+            var loggers = Logger.Factory.GetAll();
+            Logger.NotifyListeners(loggers);
+        }
+
+        static void ConfigureKissLog()
+        {
+            KissLogConfiguration.Listeners
+                .Add(new RequestLogsApiListener(new Application(ConfigurationManager.AppSettings["KissLog.OrganizationId"], ConfigurationManager.AppSettings["KissLog.ApplicationId"]))
+                {
+                    ApiUrl = ConfigurationManager.AppSettings["KissLog.ApiUrl"],
+                    UseAsync = false
+                });
+
+            KissLogConfiguration.Listeners
+                .Add(new LocalTextFileListener(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs")));
+
+            // optional KissLog configuration
+            KissLogConfiguration.Options
+                .AppendExceptionDetails((Exception ex) =>
+                {
+                    StringBuilder sb = new StringBuilder();
+
+                    if (ex is NullReferenceException nullRefException)
+                    {
+                        sb.AppendLine("Important: check for null references");
+                    }
+
+                    return sb.ToString();
+                });
+
+            // KissLog internal logs
+            KissLogConfiguration.InternalLog = (message) =>
+            {
+                Debug.WriteLine(message);
+            };
+        }
+
+        static string CreateMessage()
+        {
+            string applicationId = ConfigurationManager.AppSettings["KissLog.ApplicationId"];
+            string logsUrl = $"https://kisslog.net/RequestLogs/{applicationId}/kisslogsampleapp";
+            string textLogs = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs");
+
+            var sb = new StringBuilder();
+            sb.AppendLine("KissLog.net logs:");
+            sb.AppendLine(logsUrl);
+            sb.AppendLine();
+            sb.AppendLine("File logs:");
+            sb.AppendLine(textLogs);
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ src/netframework_ConsoleApp/netframework_ConsoleApp/Services/FooService.cs
@@ -0,0 +1,19 @@
+﻿using KissLog;
+using System;
+
+namespace netframework_ConsoleApp.Services
+{
+    internal class FooService : IFooService
+    {
+        private readonly IKLogger _logger;
+        public FooService(IKLogger logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public void Foo()
+        {
+            _logger.Debug("Foo executed");
+        }
+    }
+}
